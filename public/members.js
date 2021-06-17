1c377a3cab8b1574aa01eb29e9f528d3d3107fdc
Toualbia intissar <intissartoualbia@gmail.com>
2021-06-17 23:49:04 +0100

    first commit

---
+++ public/members.js
@@ -0,0 +1,7 @@
+const members = [
+  { id: 1, nom: "intissar", age: "27" },
+  { id: 2, nom: "linda", age: "27" },
+  { id: 3, nom: "waf", age: "27" },
+  { id: 4, nom: "christine", age: "27" },
+]
+module.exports=members;
