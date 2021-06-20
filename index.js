7817e511d627fe94a113e989e00e5f8c3e71b04b
Toualbia intissar <intissartoualbia@gmail.com>
2021-06-20 01:10:56 +0100

    first commit

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-const express = require('express');
-const app=express();
-const path = require('path');
+const express = require("express");
+const app = express();
+const path = require("path");
 
-
-app.use(message=(req,res,next)=>{
+app.use(
+  (message = (req, res, next) => {
     let day = new Date().getDay();
-    if (day>=1 && day<=5)
-    {(next())}
-    else
-    {res.send("Site is out of working days")}
-
-})
-app.use(message=(req,res,next)=>{
+    if (day >= 1 && day <= 5) {
+      next();
+    } else {
+      res.send("Site is out of working days");
+    }
+  })
+);
+app.use(
+  (message = (req, res, next) => {
     let hour = new Date().getHours();
-    if (hour>=9 && hour<=23)
-    {(next())}
-    else
-    {res.send("Site is out of working days")}
+    if (hour >= 9 && hour <= 17) {
+      next();
+    } else {
+      res.send("Site is out of working days");
+    }
+  })
+);
+app.use(express.static(path.join("./public")));
 
-})
-app.use(express.static(path.join('./public')))
-
-
-const PORT=process.env.PORT ||5000;
-app.listen(PORT,(err)=>err?console.error(err):console.log(`okey server is running! on PORT ${PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, (err) =>
+  err
+    ? console.error(err)
+    : console.log(`okey server is running! on PORT ${PORT}`)
+);
